a9da6f65ef06e437c1790ea9f532766b910326d3
AmrMekhemar <66344371+AmrMekhemar@users.noreply.github.com>
2026-09-25 19:47:49 +0300

    Increment version name and code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rased.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
         testInstrumentationRunner = "com.rased.app.XlsxRegressionInstrumentation"
     }
 
