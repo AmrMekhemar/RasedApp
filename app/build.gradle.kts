9803272584a54ae7f7af09712eb85bf632e73768
AmrMekhemar <66344371+AmrMekhemar@users.noreply.github.com>
2026-09-26 01:09:57 +0300

    v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rased.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.9"
+        versionCode = 11
+        versionName = "1.1"
         testInstrumentationRunner = "com.rased.app.XlsxRegressionInstrumentation"
     }
 
